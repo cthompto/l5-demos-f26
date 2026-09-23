503f0b8affd3eda1cab010d43bfe80b5ddd4c13d
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-09-23 14:08:10 -0400

    add more demos

--- demos/pixel1.lua
+++ demos/pixel1.lua
@@ -19,9 +19,11 @@ function setup()
 
   img = loadImage('assets/red-leaf-600-900.jpg')
   image(img, 0, 0, width, height)
-  limit = ((width/10)*(height/10))
-  x = 5
-  y = 5
+
+  pixelSize = 10
+  limit = ((width/pixelSize)*(height/pixelSize))
+  x = pixelSize/2
+  y = pixelSize/2
   for i=1, limit do
     g = get(x,y)
     print(g)
@@ -31,12 +33,11 @@ function setup()
       y = y
     }
     table.insert(myPixels, newPixel)
-    y = y + 10;
-    if y == height+5 then
-      y = 5;
-      x = x + 10;
+    y = y + pixelSize;
+    if y == height+(pixelSize/2) then
+      y = pixelSize/2;
+      x = x + pixelSize;
     end
-    
   end
 end
 
@@ -45,12 +46,12 @@ function draw()
  image(img, 0, 0, width, height)
 
  for i=1, #myPixels do
-  if (mouseX - myPixels[i].x)*(mouseX - myPixels[i].x) + (mouseY - myPixels[i].y)*(mouseY - myPixels[i].y) >= 60*60 then
+  if (mouseX - myPixels[i].x)*(mouseX - myPixels[i].x) + (mouseY - myPixels[i].y)*(mouseY - myPixels[i].y) <= 50*50 then
     fill(myPixels[i].color)
-    rect(myPixels[i].x, myPixels[i].y, 10, 10)
-  elseif (mouseX - myPixels[i].x)*(mouseX - myPixels[i].x) + (mouseY - myPixels[i].y)*(mouseY - myPixels[i].y) >= 50*50 then
+    rect(myPixels[i].x, myPixels[i].y, pixelSize, pixelSize)
+  elseif (mouseX - myPixels[i].x)*(mouseX - myPixels[i].x) + (mouseY - myPixels[i].y)*(mouseY - myPixels[i].y) <= 60*60 then
     fill(myPixels[i].color[1],myPixels[i].color[2],myPixels[i].color[3],myPixels[i].color[4]-100)
-    rect(myPixels[i].x, myPixels[i].y, 10, 10)
+    rect(myPixels[i].x, myPixels[i].y, pixelSize, pixelSize)
   end
  end
 
@@ -60,43 +61,7 @@ end
 -- Things below here are notes, tests, and example code
 
 --[[
-if (mouseX - myPixels[i].x)*(mouseX - myPixels[i].x) + 
-(mouseY - myPixels[i].y)*(mouseY - myPixels[i].y) <= 20*20 then
 
 
 
-if ((x - circle_x) * (x - circle_x) +
-        (y - circle_y) * (y - circle_y) <= rad * rad)
-
-
---
-circles = {
-  {x = 100, y = 100, speedX = 2, speedY = 1},
-  {x = 200, y = 150, speedX = -1, speedY = 2},
-  {x = 150, y = 200, speedX = 1, speedY = -2}
-}
-
---
-
-circles = {}
-
-function setup()
-  size(300, 300)
-
-  -- Create 10 random circles
-  for i = 1, 10 do
-    newCircle = {
-      x = random(width),
-      y = random(height),
-      speedX = random(-3, 3),
-      speedY = random(-3, 3),
-      size = random(10, 40)
-    }
-    table.insert(circles, newCircle)
-  end
-end
-
-function draw()
-   -- Draw circle
-    circle(circles[i].x, circles[i].y, 30)
 ]]--
