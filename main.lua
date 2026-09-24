0ca249ed920331a6e76a3d05e03a8b0227e02d45
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-09-24 18:27:44 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,30 @@
--- Cursor Cords
+-- Anatomy of a Sketch
 -- Chelsea Thompto
 
+-- Connects L5 library to sketch code.
 require("L5")
 
+-- Function to set up sketch. (All functions end with an "end")
 function setup()
+
+  -- Sets window size.
   size(400, 400)
   
+  -- Describes sketch visuals.
   windowTitle("Cursor Cords Demo")
-  
 end
 
+-- Function to draw in the window. (Runs 30 times per second)
 function draw()
+
+  -- Fills the whole screen with a color. (Essential to avoid ghost images)
   background(140, 237, 237)
-  -- the code below adds the cursor position for bebugging
+
+  -- A basic shape in white.
+  fill(255)
+  ellipse(200,200,100,100)
+
+  -- The code below adds a cursor position for bebugging.
   fill(0)
   text("X: "..mouseX.."  Y: "..mouseY, mouseX+5,mouseY+30)
 end
